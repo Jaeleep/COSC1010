82b00b495c990441566aa702333a029a0054c173
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-09-15 15:21:08 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,40 @@
 #
 # Jae Lee Park
-# Sep 15, 2024
-# Hot Dog Cookout Calculator Programming Project
+# Sep 14,2024
+# Areas of Rectangles Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+# Local variables
+lenghthA = 0 # The length of rectangle A
+widthA = 0 # The width of rectangle A
+lengthB = 0 # The length of rectagnle B
+widthB = 0 # The width of rectangle B
+areaA = 0 # The area of rectangle A
+areaB = 0 # The area of rectangle B
 
-# Constants
-HOT_DOGS_PER_PACKAGE = 10
-BUNS_PER_PACKAGE = 8
+# Get length A
+lengthA = float(input('Engter the length of rectangle A: '))
 
-#Local variables
-numAttending = 0 # The number of people attending
-numPerperson = 0 # The number of hot dogs and buns per person
-total = 0 # The total number of hot dogs and buns needed
-minDogs = 0 # The minimum number of packages of hot dogs 
-minBuns = 0 # The minimum number of packages of hot dog buns
-dogsLeft = 0 # The numberof hot dogs left over from a package
-bunsLeft = 0 # The number of hot dog buns left over from a package
+# Get width A
+widthA = float(input('Engter the width of rectangle A: '))
 
-# Get the number of people attending the cookout from the user.
-numAttending = int(input('Enter the number of people attending the cookout: '))
+# Get length B
+lengthB = float(input('Engter the length of rectangle B: '))
 
-# Get the number of hot dogs per person from the user.
-numPerperson = int(input('Enter the number of hot dogs per person: '))
+# Get width B
+widthB = float(input('Engter the width of rectangle B: '))
 
-# Calculate the total number of hot dogs and buns needed.
-total = numAttending * numPerperson
+# Calculate area A
+areaA = lengthA * widthA
 
-# Calculate the minimum number of packages of hot dogs needed.
-minDogs = total // HOT_DOGS_PER_PACKAGE
+# Calculate area B
+areaB = lengthB * widthB
 
-# Determine if the number of people attending is larege enough to require more than one package of hot dogs.
-if minDogs > 0 :
-    #Calculate the number of hot dogs left over from a package if any.
-    dogsLeft = total % HOT_DOGS_PER_PACKAGE
-
-    #If there will be left overs, add an additional package of hot dogs
-    if dogsLeft != 0:
-        minDogs += 1
-
-# Set the minimum number of packages of hot dogs to one becuase the numberof people attending is small enough to requrie only one package of hot dogs.
-else: 
-    minDogs = 1
- 
-# Determine the number of lefover hot dogs, if any.
-dogsLeft = (HOT_DOGS_PER_PACKAGE * minDogs) - total 
-
-# Calculate the minimum number of packages of buns needed.
-minBuns = (total + BUNS_PER_PACKAGE - 1) //BUNS_PER_PACKAGE
-
-#Determine the number of lefover buns.
-bunsLeft = (BUNS_PER_PACKAGE * minBuns) - total 
-
-# Display the minimum packages of hot dogs needed.
-print('Minimum packages of hot dogs nedded: ', minDogs)
- 
-# Display the minimum packages of hot dog buns needed.
-print('Minimum packages of hot dog buns nedded: ', minBuns)
-
-# Display the number of hot dogs left over.
-print('Hot dogs left over: ', dogsLeft)
-
-# Display the number of hot dog buns left over.
-print('Hot dog buns left over: ', bunsLeft)
+# Print area comparison using if-elif-else statements
+if areaA > areaB:
+    print('\nRectangle A has a greater area.')
+elif areaB > areaA:
+    print('\nRectangle B has a greater area.')
+else:
+    print('\n4
+    Both rectangles have the same area.')
