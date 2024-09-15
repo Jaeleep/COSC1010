ba60fd1714df0fa0a74afbe7a26ef1820e898009
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-09-15 14:38:41 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 #
 # Jae Lee Park
-# Sep 8, 2024
-# Sales Tax Programming Project
+# Sep 7, 2024
+# Sales Prediction Programming Project
 # COSC 2409 DNT
 #
 
-# Variable declarations
-amountofpurchase = 0.0
-state_tax = 0.0
-county_tax = 0.0
-total_tax = 0.0
-total_sale = 0.0
+# Variables to hold the sales total and the profit
+total_sales = 0.0
+profit = 0.0
 
-# Constants for the state and county tax rates
-STATE_TAX_RATE = 0.05
-COUNTY_TAX_RATE = 0.025
-# Get the amount of the purchase.
-amountofpurchase = float(input('Enter the amount of purchase:'))
+# Get the amount of projected sales.
+total_sales = float(input('Enter the projected sales:'))
 
-# Calculate the state sales tax.
-state_tax = amountofpurchase * STATE_TAX_RATE
+# Calculate the projected profit.
+profit = total_sales * 0.23
 
-# Calculate the county sales tax.
-county_tax = amountofpurchase * COUNTY_TAX_RATE
-
-# Calculate the total tax.
-total_tax = state_tax + county_tax
-
-# Calculate the total of the sale.
-total_sale = amountofpurchase + total_tax
-
-# Print information about the sale.
-print('Amount of purchase $' + format(amountofpurchase, ',.2f'),\
-    'State tax: $' + format(state_tax ,',.2f'),\
-    'County tax: $' + format(county_tax ,',.2f'),\
-    'Total tax: $' + format(total_tax ,',.2f'),\
-    'Total sale: $' + format(total_sale ,',.2f'), sep = '\n')
+# Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
