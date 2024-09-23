fc4c721edd6ddd401a256cf61f81810aaa0a5441
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-09-22 23:47:59 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #
 # Jae Lee Park
-# Sep 17, 2024
-# Bug Collector Programming Project
+# Sep 22, 2024
+# Budget Analysis Programming Project
 # COSC 2409 DNT
 #
-# Initialize variables for bugs and total number of bugs collected.
-total_bugs = 0 
+# Use comments liberally throughout the program. 
+#Get the budgeted amount from the user.
+budget = float(input('Enter the amount you have budgeted for the month:'))
 
-# Get number of bugs collected each day using a for loop.
-for  day in range(1, 6):
-    print('Enter the bugs collected on day', day)
-    bugs = int(input())
-    total_bugs += bugs
+# Initialize total expenses
+total_expenses = 0
 
-# Display the total number of bugs collected.
-print(f'Total number of bugs collected over 5 days: {total_bugs}')
+# Ask the user to enter expenses and keep a running total
+more_expenses = 'y'
+
+while more_expenses.lower() == 'y':
+    expense = float(input('Enter an expensse: '))
+    total_expenses += expense
+
+    # Ask the user if they want to add another expense.
+    more_expenses = input('Do you want to add another expense?: Type y for yes, n for no: ')
+
+# Calculate if the user is over or under budget.
+difference = budget - total_expenses
+
+# Display the results to the user.
+if difference > 0:
+    print('You are under budget by $', round(difference, 2))
+elif difference < 0:
+    print('You are over budget by $', round(-difference, 2))
+else:
+    print('You have exactly met your budget.')
