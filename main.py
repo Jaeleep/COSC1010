dbd113ecc3c64143e3570383f3d2f38b65f8e7ca
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-10-15 12:50:26 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 #
 # Jae Lee Park
-# Sep 26, 2024
-# Feet to Inches Programming Project
+# Oct 15, 2024
+# File Display Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program.
+# Use comments liberally throughout the program. 
+# Declare all local variables
+myfile = 0
+line = 0
+number = 0
 
-#Initialize the conversion factor
-inches_per_foot = 12
+# Open the file.
+myfile = open('numbers.txt', 'r')
 
-# Function to convert feet to inches
-def feet_to_inches(feet):
-    return feet * inches_per_foot
+# Read and display the file's contents.
+for line in myfile:
+    number = int(line)
+    print(number)
 
-# Get the number of feet from the user
-feet = int(input('Enter the number of feet: '))
-
-# Calculate the distance in inches using the conversion function
-inches = feet_to_inches(feet)
-
-# Display the result
-print(feet, 'feet equals', inches, 'inches')
+# Closse the file.
+myfile.close()
