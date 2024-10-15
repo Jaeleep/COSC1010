ce6e29d7821f0190a73611f28504a0b96bb98ed7
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-10-15 17:12:19 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 #
 # Jae Lee Park
 # Sep 26, 2024
-# Feet to Inches Programming Project
+# Kilometer Converter Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program.
 
-#Initialize the conversion factor
-inches_per_foot = 12
-feet = 0
-inches = 0
+# Declare the variables
+kilometers = 0
+miles = 0
+conversion_factor = 0.6214
 
+# Get the distance in kilometers from the user
+kilometers = float(input('Enter the distance in kilometers: '))
 
-# Function to convert feet to inches
-def feet_to_inches(feet):
-    return feet * inches_per_foot
-
-# Get the number of feet from the user
-feet = int(input('Enter the number of feet: '))
-
-# Calculate the distance in inches using the conversion function
-inches = feet_to_inches(feet)
+# Calculate the distance in miles using the conversion formula
+miles = kilometers * conversion_factor
 
 # Display the result
-print(feet, 'feet equals', inches, 'inches')
+print(kilometers, 'kilometers euqals', miles, 'miles.')
