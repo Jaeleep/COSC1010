14752b5d9d4d04fbc0a3be36e50b76cb11c4ed3d
Jaeleep <151694967+Jaeleep@users.noreply.github.com>
2024-10-15 17:18:09 -0600

    Add files via upload

---
+++ Feet-to-Inches/main.py
@@ -0,0 +1,26 @@
+#
+# Jae Lee Park
+# Sep 26, 2024
+# Feet to Inches Programming Project
+# COSC 2409 DNT
+#
+# Use comments liberally throughout the program.
+
+#Initialize the conversion factor
+inches_per_foot = 12
+feet = 0
+inches = 0
+
+
+# Function to convert feet to inches
+def feet_to_inches(feet):
+    return feet * inches_per_foot
+
+# Get the number of feet from the user
+feet = int(input('Enter the number of feet: '))
+
+# Calculate the distance in inches using the conversion function
+inches = feet_to_inches(feet)
+
+# Display the result
+print(feet, 'feet equals', inches, 'inches')
